a77ee94aeb246c4488804f5a101f518324be684b
Witthawin Sirisiwaphak <eing.wi@gettgo.com>
2020-10-04 18:37:46 +0700

    add text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,49 @@
 <template>
-  <div class="home" :style="styleBg" @click="changeToggle">
-  </div>
+<div>
+  <fullscreen ref="fullscreen" @change="fullscreenChange">
+    <div class="home" :style="styleBg" @click="changeToggle">
+      {{styleText}}
+    </div>
+    <div type="button" @click="toggles" class="toogleBtn">{{toggleText}}</div>
+
+  </fullscreen>
+</div>
 </template>
 
 <script lang="js">
 import { Vue } from 'vue-property-decorator';
+import fullscreen from 'vue-fullscreen';
+
+Vue.use(fullscreen);
 
 export default Vue.extend({
   data: () => ({
     toggle: false,
+    fullscreen: false,
   }),
   computed: {
     styleBg() {
       if (this.toggle) { return { backgroundColor: '#3C413D' }; }
       return { backgroundColor: '#37433a' };
     },
+    styleText() {
+      if (this.toggle) return '#3C413D';
+      return '#37433a';
+    },
+    toggleText() {
+      if (this.fullscreen) return 'Exit fullscreen';
+      return 'Click fullscreen';
+    },
   },
   methods: {
     changeToggle() {
       this.toggle = !this.toggle;
+    },
+    toggles() {
+      this.$refs.fullscreen.toggle(); // recommended
+    },
+    fullscreenChange(fullscreens) {
+      this.fullscreen = fullscreens;
     },
   },
 });
@@ -26,6 +51,22 @@ export default Vue.extend({
 
 <style lang="stylus">
 .home {
-  min-height :100vh;
+  min-height 100vh;
+  cursor pointer;
+  display flex;
+  justify-content center;
+  align-items center;
+  color black;
+  position relative
+}
+
+.toogleBtn {
+  background-color transparent
+  outline none
+  position absolute
+  right 40px
+  top 40px
+  color black
+  cursor pointer;
 }
 </style>
